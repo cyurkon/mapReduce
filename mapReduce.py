c22798eb8ce780a3c3fc424fe0c1aee2fde93d27
Conner Yurkon <conner.yurkon@gmail.com>
2021-06-24 00:45:38 -0400

    replace Counter __add__ w/ update, new results

--- mapReduce.py
+++ mapReduce.py
@@ -24,17 +24,25 @@ def mapper(doc, stop_words):
     return Counter([word.lower() for word in re.findall(r'[A-Za-z]{3,}', doc) if word.lower() not in stop_words])
 
 def reducer(count1, count2):
-    return count1 + count2
+    # return count1 + count2
+    # __add__ operations for Counter objects are slow because they include additional checks for negative elements
+    count1.update(count2)
+    return count1
 
 @timing
 def mapReduce(news_groups, mapper, reducer, num_cores):
+    # not really necessary, but saves threading overhead in the case of 1 core
+    if num_cores == 1:
+        mapped = map(mapper, news_groups)
+        return reduce(reducer, mapped)
     with mp.Pool(num_cores) as pool:
-        mapped = pool.map(mapper, news_groups['data'])
+        mapped = pool.map(mapper, news_groups)
         reduced = reduce(reducer, pool.map(partial(reduce, reducer), np.array_split(mapped, pool._processes)))
     return reduced
 
 def main(num_cores):
-    news_groups = fetch_20newsgroups(data_home='.', subset='all')
+    # multiply by some constant to magnify interval containing results
+    news_groups = fetch_20newsgroups(data_home='.', subset='all')['data'] * 10
     # nltk.download('stopwords', '.')
     stop_words = stopwords.words('english', '.')
     res = mapReduce(news_groups, partial(mapper, stop_words=stop_words), reducer, num_cores)
--- plot.py
+++ plot.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 
-core_times = [(1, 125.26), (4, 92.48), (8, 62.39), (16, 12.36), (32, 7.92)]
+core_times = [(1, 92.55), (2, 66.44), (4, 41.11), (8, 26.97), (16, 20.27), (32, 17.56)]
 
 plt.loglog(*zip(*core_times), marker='o', basex=2)
 plt.xlabel('Number of Cores')
-plt.ylabel('Time (s)')
+plt.ylabel('Execution Time (s)')
 plt.title('Number of Cores vs. Execution Time')
 plt.show()
